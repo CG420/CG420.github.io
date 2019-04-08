d2cdd814537a415a1337de63c97f87effbc4e9d3
Chris Grooms <cgrooms21@aol.com>
2019-04-08 14:14:59 -0500

    fixed index

--- gulpfile.js
+++ gulpfile.js
@@ -31,5 +31,28 @@ gulp.task('build-css', function(){
 });
 
 gulp.task('watch', function(){
-  gulp.watch('./public/src/scss/**/*.scss', ['build-css']);
+  gulp.watch('.//src/scss/**/*.scss', ['build-css']);
+  gulp.watch('.//src/scss/**/*.scss', ['build-resume']);
+  gulp.watch('.//src/scss/**/*.scss', ['build-contact']);
+  gulp.watch('.//src/scss/**/*.scss', ['build-thanks']);
 });
+
+gulp.task('build-resume', function(){
+  //Create an unminified version
+  var full = gulp.src([
+    'src/scss/resume.scss'
+  ])
+  . pipe(scss())
+  . pipe(concat('resume.css'))
+  . pipe(gulp.dest('dist/css'));
+
+  //Create a minified version
+  var min = gulp.src([
+    'src/scss/resume.scss'
+  ])
+  . pipe(scss())
+  . pipe(cleanCSS())
+  . pipe(concat('resume.min.css'))
+  . pipe(gulp.dest('dist/css'));
+});
+
